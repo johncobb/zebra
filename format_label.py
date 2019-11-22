daa1c36e22380bdc88fcff5dc6128aff84420f4e
John Cobb <emailjohncobb@gmail.com>
2019-11-22 21:45:43 +0000

    updated green label template

--- format_label.py
+++ format_label.py
@@ -35,10 +35,28 @@ def format_porsche_data():
     return data
 
 
+def format_danny_data():
+    f = open("template", "r")
+
+    data = f.read()
+
+    data = data.replace("{vin}", "JTMZFREV1GJ089869")
+    data = data.replace("{year}", "2016")
+    data = data.replace("{make}", "TOYOTA")
+    data = data.replace("{model}", "RAV 4")
+    data = data.replace("{color}", "METALLIC SILVER")
+    data = data.replace("{small_color}", "METALLIC SLVR")
+    data = data.replace("{stock}", "8371AD3A")
+    data = data.replace("{mileage}", "35,630 MILES")
+    data = data.replace("{entry_date}", "JAN 05")
+    data = data.replace("{dealership}", "")
+
+    return data
 if __name__ == "__main__":
 
     # label = format_data()
-    label = format_porsche_data()
+    label = format_danny_data()
+    # label = format_porsche_data()
     print(label)
 
 # print(data)
